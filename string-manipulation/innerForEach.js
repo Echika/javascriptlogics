77abdeed600dd563e3e4ae10a5f06c22c4276336
Echika <echikaraphael@gmail.com>
2025-02-04 21:57:24 +0100

    transformed element

---
+++ string-manipulation/innerForEach.js
@@ -0,0 +1,10 @@
+function innerForEach(str) {
+  const brokenDownStr = str.split(" ");
+  if (brokenDownStr.length <= 1) {
+    return "Input more string to make a longer sentence";
+  }
+  for(let breakDown of brokenDownStr){
+    return breakDown = breakDown.split('').sort()
+ }
+
+}
