86efa8f7d470fb7d0696b6e6268cd1e6ad74762c
Echika <echikaraphael@gmail.com>
2025-02-04 22:02:40 +0100

    transformed element

--- string-manipulation/innerForEach.js
+++ string-manipulation/innerForEach.js
@@ -1,10 +1,12 @@
-function innerForEach(str) {
-  const brokenDownStr = str.split(" ");
-  if (brokenDownStr.length <= 1) {
-    return "Input more string to make a longer sentence";
+// A function that takes a string as input but returns the string sorted in alphabetical
+function  transformElements(str){
+  const splittedStr = str.split(' ');
+  if(splittedStr.length <= 1){
+      return 'Enter a longer sentence for better performance';
   }
-  for(let breakDown of brokenDownStr){
-    return breakDown = breakDown.split('').sort()
- }
-
+  return (splittedStr.map(elem => elem.split('').sort().join(''))).join(' ')
 }
+
+//testing
+transformElements('i love my mother so much i donot know what you think about that. do you love your mother too')
+//'i elov my ehmort os chmu i dnoot know ahtw ouy hiknt abotu .ahtt do ouy elov oruy ehmort oot'
