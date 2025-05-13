5efba37c1a39c54e216fcd362caeff52cb816cf4
Echika <echikaraphael@gmail.com>
2025-05-13 13:49:33 +0100

    Fixed Error

--- objects-manipulation-logics/identicalObjects.js
+++ objects-manipulation-logics/identicalObjects.js
@@ -1,16 +1,36 @@
-// A function to check if two objects are the same
-function compare(obj1, obj2, objName){
-    (obj1[objName] === obj2[objName]) ? true : false
+// A function to check if two objects are equal
+function areTheyEqual(obj1, obj2) {
+  let obj1Key = Object.keys(obj1).sort();
+  let obj2Key = Object.keys(obj2).sort();
+
+  // Check if they have same length
+  if (obj1Key.length !== obj2Key.length) {
+    return "Objects are not the same!";
+  }
+  // Check if they have same property names
+  for (let i = 0; i < obj1Key.length; i++) {
+    if (obj1Key[i] !== obj2Key[i]) {
+      return "Objects are not the same";
+    }
+  }
+  //Check if they have same values
+  for (let key of obj1Key) {
+    if (obj1[key] !== obj2[key]) {
+      return "Objects are not the same";
+    }
+  }
+  return "Objects are the same";
 }
 
-const object1 = {
-    name: 'goat',
-    color: 'black'
+const person1 = {
+    name: 'love',
+    age: 45,
+    height: '5feet'
+}
+const person2 = {
+    name: 'love',
+    age: 4,
+    height: '5feet'
 }
 
-const object2 = {
-    name: 'dog',
-    color: 'brown'
-}
-
-console.log(obj1, obj2, 'name')
+console.log(areTheyEqual(person1, person2))
